49aefbb5e18abcd2a133f3315582edd767eb0f4d
Muhamad Nur Rasyid <nurrasyid186@gmail.com>
2026-04-13 21:24:10 +0700

    Make map-reduce.sh executable and run it

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 chmod +x hadoop-deploy.sh
 chmod +x hadoop-lxc-cluster.sh
+chmod +x map-reduce.sh
 chmod +x apache-spark-install.sh
 chmod +x hadoop-apache-hive.sh
 
 ./hadoop-deploy.sh
 ./hadoop-lxc-cluster.sh
+./map-reduce.sh
 ./apache-spark-install.sh
 ./hadoop-apache-hive.sh
