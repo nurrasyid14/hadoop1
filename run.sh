c00152ff173a622812d6f461406f05000c3c7317
Muhamad Nur Rasyid <nurrasyid186@gmail.com>
2026-04-27 00:59:25 +0700

    Run.sh

--- run.sh
+++ run.sh
@@ -3,9 +3,11 @@ chmod +x hadoop-lxc-cluster.sh
 chmod +x map-reduce.sh
 chmod +x apache-spark-install.sh
 chmod +x hadoop-apache-hive.sh
+chmod +x hbase_sqoop_install.sh
 
 ./hadoop-deploy.sh
 ./hadoop-lxc-cluster.sh
 ./map-reduce.sh
 ./apache-spark-install.sh
 ./hadoop-apache-hive.sh
+./hbase_sqoop_install.sh
